a7424f989934f82c8a7b9a5956e74b692b6b1172
SANTIAGO <santiagomunoz298536@correo.itm.edu.co>
2022-02-16 12:36:08 -0500

    finish date class

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using POOConcepts;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+try
+{
+    Console.WriteLine(new Date(2022, 2, 10));
+    Console.WriteLine(new Date(1974, 12, 23));
+    Console.WriteLine(new Date(2023, 11, 9));
+    Console.WriteLine(new Date(2024, 2, 29));
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.Message);
+}
